12bbb290ff136c7d1243f340d69d17735e0abb6e
erashu122 <kbitu8676@gmail.com>
2025-03-03 19:48:02 +0530

    initial commit

--- ex6.py
+++ ex6.py
@@ -1,3 +1,6 @@
 name = input("enter your name:")
 result = len(name)
+f= input("enter the character you want to find:")
+result=name.find(f)
 print(f"your name has {result} characters")
+print(f"your finding position is {result}")
